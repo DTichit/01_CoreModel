1424c98d745c331ece7e06dd4833b87505678c85
DTichit <dtichit@deloitte.fr>
2023-07-04 11:47:45 +0200

    Clean functions

--- _src/_functions/download_file.py
+++ _src/_functions/download_file.py
@@ -10,11 +10,6 @@ import xlwings as xw
 
 class DownloadFile:
 
-    # Attributes
-    #url = "https://www.insee.fr/fr/statistiques/fichier/7625852/136_IR_Biens.xlsx"
-    #type = "xlsx"
-    #dwnld_path = "C:\Users\DTichit\Downloads"
-
     # Class
     def __init__(self):
         self.arg1 = 1
